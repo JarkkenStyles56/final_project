7032e77d912cf2609e387d1757bd9e8262aed408
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2021-01-23 13:07:33 -0700

    Added Button to all the results. The button will be pressed to mvoe the result to the collection

--- client/src/components/ComicBookImages.js
+++ client/src/components/ComicBookImages.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './ComicBookImages.css';
 
 
 const ComicBookImages = (props) => {
+
+    const styles = {
+        comicImage: {
+            backgroundColor: 'blue'
+        }
+        
+    };
+
     return (
         <div>
             {props.img.map((item) => {
                 return (
-                    <img src={item} alt="" heigth={200} width={200} />
+                    <div key={item} style={styles.comicImage} heigth={200} width={200}>
+                        <img src={item} alt="" heigth={200} width={200} />
+                        <br />
+                        <button color='blue' type='submit'>Add to Collection</button>
+                    </div>
                 );
             })}
         </div>
