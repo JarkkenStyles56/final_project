054b9fa5946d9aa03a1e4767cea76e6ada6ed523
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2021-01-26 22:32:30 -0700

    Collection page up and running. Need to fix up styling somewhat

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,34 +1,48 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function Collection(props) {
-    const testData = [
-        {
-            comic: {
-                title: 'The Amazing Spider Man',
-                image: 'https://i.annihil.us/u/prod/marvel/i/mg/4/50/5192af7c78397/detail.jpg',
-                issue: '2'
-            }
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        fetchCollection();
+    }, []);
+    function fetchCollection() {
+        axios.get('/api/collected')
+            .then(({ data }) => {
+                setResults(data);
+            });
+    }
+    // const testData = [
+    //     {
+    //         comic: {
+    //             title: 'The Amazing Spider Man',
+    //             image: 'https://i.annihil.us/u/prod/marvel/i/mg/4/50/5192af7c78397/detail.jpg',
+    //             issue: '2'
+    //         }
 
-        },
-        {
-            comic: {
-                title: 'Spider Ham',
-                image: 'https://i.annihil.us/u/prod/marvel/i/mg/c/10/5c0a98ad53dc2/clean.jpg',
-                issue: '50'
-            }
-        }
-    ];
+    //     },
+    //     {
+    //         comic: {
+    //             title: 'Spider Ham',
+    //             image: 'https://i.annihil.us/u/prod/marvel/i/mg/c/10/5c0a98ad53dc2/clean.jpg',
+    //             issue: '50'
+    //         }
+    //     }
+    // ];
     return (
         <div>
-            {testData.map(collected => {
+            {results.map(collected => {
 
                 const { comic } = collected;
 
                 return (
-                    <div>
-                        <h2>{comic.title}</h2>
-                        <img src={comic.image} height={250} width={250} alt={comic.title}></img>
-                        <h3>{comic.issue}</h3>
+                    <div key={comic.id}>
+                        <img src={comic.image.small_url} alt="" heigth={200} width={200} />
+                        <p>Issue #: {comic.issue_number}</p>
+                        <p>Title: {comic.name}</p>
+                        <p>Cover Date: {comic.cover_date}</p>
+                        {/* <button onClick={() => addToCollection(item)} color='blue' type='submit'>Add to Collection</button> */}
+                        <br />
                     </div>
                 );
             })}
